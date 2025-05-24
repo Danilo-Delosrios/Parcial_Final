fac016c51a2eaa64c8bf513a4b6d90e5e8dc0315
Danilo <daniloesteban.delosriosblanquicet@unitecnar.edu.co>
2025-05-24 18:18:09 -0500

    Add files via upload

---
+++ parcial3/src/Formularios/Main.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Formularios;
+
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        SwingUtilities.invokeLater(() -> {
+            frmMatriz matriz = new frmMatriz();
+            matriz.setVisible(true);
+        });
+    }
+}
