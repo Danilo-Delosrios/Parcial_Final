7c2d93cef8d19e1d403c57e66dcebdeec0db172d
Danilo <daniloesteban.delosriosblanquicet@unitecnar.edu.co>
2025-05-24 18:01:05 -0500

    Add files via upload

---
+++ PARCIAL FINAL/src/parcial/pkgfinal/Parcial_Final.java
@@ -0,0 +1,13 @@
+package parcial.pkgfinal;
+
+import Formularios.FrmMatriz;
+
+public class Parcial_Final {
+
+    public static void main(String[] args) {
+         FrmMatriz formulario = new FrmMatriz();
+            formulario.setVisible(true);
+        }
+    }
+    
+
